45975f97ea187299e3613481578fd99f106e536f
PiotrStoklosa <PiotrJStoklosa@gmail.com>
2025-03-10 11:40:06 +0100

    Remove SystemClassLoaderSearch

--- src/main/java/org/threadmonitoring/ThreadAgentEntry.java
+++ src/main/java/org/threadmonitoring/ThreadAgentEntry.java
@@ -11,15 +11,13 @@ import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.jar.JarFile;
 import java.util.stream.Stream;
 
 public class ThreadAgentEntry {
     public static void premain(String agentArgs, Instrumentation inst) throws URISyntaxException, IOException, ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+
         URI uri = AgentClassLoader.class.getProtectionDomain().getCodeSource().getLocation().toURI();
-        System.out.println("URI: " + uri);
         Path lib = Paths.get(uri).getParent().resolve("./lib");
-        System.out.println("lib: " + lib);
 
         try (Stream<Path> pathStream = Files.list(lib)) {
             URL[] urls = pathStream.map(Path::toUri).map(uri1 -> {
@@ -29,19 +27,12 @@ public class ThreadAgentEntry {
                     throw new RuntimeException(e);
                 }
             }).toArray(URL[]::new);
-            for (URL url : urls) {
-                System.out.println(url);
-            }
+
             AgentClassLoader agentClassLoader = new AgentClassLoader(urls, Thread.currentThread().getContextClassLoader());
+            Thread.currentThread().setContextClassLoader(agentClassLoader);
             Class<?> agentMain = agentClassLoader.loadClass("org.threadmonitoring.Main");
             Method run = agentMain.getDeclaredMethod("run", Instrumentation.class);
 
-            String jarPath1 = Paths.get(uri).getParent().resolve("C:\\Users\\Piotr\\OneDrive\\Pulpit\\Studia\\Magisterka\\production\\lib\\thread-agent-1-0.jar").toString();
-            String jarPath2 = Paths.get(uri).getParent().resolve("C:\\Users\\Piotr\\OneDrive\\Pulpit\\Studia\\Magisterka\\production\\thread-agent.jar").toString();
-            System.out.println(jarPath1);
-            inst.appendToSystemClassLoaderSearch(new JarFile(jarPath1));
-            System.out.println(jarPath2);
-            inst.appendToSystemClassLoaderSearch(new JarFile(jarPath2));
 
             run.invoke(null, inst);
         }
