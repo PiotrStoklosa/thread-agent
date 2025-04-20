e9c47399da343a25056d9016b459311fd61b9ca3
PiotrStoklosa <PiotrJStoklosa@gmail.com>
2025-04-20 23:00:26 +0200

    Improve logging and performance for lock and unlock

--- src/main/java/org/threadmonitoring/advices/UnlockAdvice.java
+++ src/main/java/org/threadmonitoring/advices/UnlockAdvice.java
@@ -3,8 +3,11 @@ package org.threadmonitoring.advices;
 import net.bytebuddy.asm.Advice;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.threadmonitoring.configuration.Configuration;
 
+import java.util.List;
 import java.util.concurrent.locks.Lock;
+import java.util.stream.Collectors;
 
 public class UnlockAdvice {
 
@@ -18,15 +21,30 @@ public class UnlockAdvice {
     public static void interceptEntry(
             @Advice.This Lock lock
     ) {
-        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
-        for (StackTraceElement stackTraceElement : stackTrace) {
-            if (stackTraceElement.getClassName().contains("java.util.concurrent.ThreadPoolExecutor") ||
-                    stackTraceElement.getClassName().contains("org.apache.logging.log4j")) {
+        StackWalker walker = StackWalker.getInstance(StackWalker.Option.RETAIN_CLASS_REFERENCE);
+
+        List<StackWalker.StackFrame> frames = walker.walk(stream -> stream.collect(Collectors.toList()));
+
+        for (int i = 0; i < frames.size(); i++) {
+            Class<?> cls = frames.get(i).getDeclaringClass();
+
+            if (Lock.class.isAssignableFrom(cls) && frames.get(i).getMethodName().equals("unlock")) {
+
+                List<StackWalker.StackFrame> callers = frames.subList(i + 1, frames.size());
+
+                boolean allFromMonitored = callers.stream().allMatch(f ->
+                        Configuration.monitoredPackages.stream()
+                                .anyMatch(pkg -> f.getClassName().startsWith(pkg)) ||
+                                f.getClassName().equals("java.lang.Thread")
+                );
+
+                if (allFromMonitored && !callers.isEmpty()) {
+                    LOGGER.info("Lock {} released by thread {} at {}",
+                            lock.toString(), Thread.currentThread().getName(), callers.get(0));
+                }
                 return;
             }
         }
-        LOGGER.info("Lock {} released by thread {} at {}",
-                lock.toString(), Thread.currentThread().getName(), stackTrace[stackTrace.length - 1]);
 
     }
 
