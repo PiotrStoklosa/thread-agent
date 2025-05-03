538822b72275a5cec84a558f9fca994be2010aa4
PiotrStoklosa <PiotrJStoklosa@gmail.com>
2025-05-03 15:55:00 +0200

    Remove unused code

--- src/main/java/org/threadmonitoring/substitution/call/GeneralSubstitution.java
+++ src/main/java/org/threadmonitoring/substitution/call/GeneralSubstitution.java
@@ -3,7 +3,7 @@ package org.threadmonitoring.substitution.call;
 import org.threadmonitoring.substitution.LoggingNotifier;
 
 public class GeneralSubstitution {
-    public static void substitute2(String method) throws InterruptedException {
+    public static void substitute2(String method) {
         LoggingNotifier.log(
                 "Thread " + Thread.currentThread().getName() + " called " + method
                 , GeneralSubstitution.class
