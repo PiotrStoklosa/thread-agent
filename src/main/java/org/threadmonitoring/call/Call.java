759bc5f508eceb67d6059e3973a4d5fd7f5b8db4
PiotrStoklosa <PiotrJStoklosa@gmail.com>
2025-05-03 16:01:38 +0200

    Add synchronized call substitute support

---
+++ src/main/java/org/threadmonitoring/call/Call.java
@@ -0,0 +1,5 @@
+package org.threadmonitoring.call;
+
+public class Call {
+    public static void alertMultithreadingCall(String method) {}
+}
---
+++ src/main/java/org/threadmonitoring/call/SynchronizedCall.java
@@ -0,0 +1,9 @@
+package org.threadmonitoring.call;
+
+public class SynchronizedCall {
+    public static void alertSynchronizedEntry() {
+    }
+
+    public static void alertSynchronizedExit() {
+    }
+}
