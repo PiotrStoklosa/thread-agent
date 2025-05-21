4949041bfef4f31737f1a26617c7dce92590bb46
PiotrStoklosa <PiotrJStoklosa@gmail.com>
2025-05-21 20:21:55 +0200

    Add object to synchronized substitution
    add method to monitor thread before synchronized entry

--- src/main/java/org/threadmonitoring/substitution/call/SynchronizedCall.java
+++ src/main/java/org/threadmonitoring/substitution/call/SynchronizedCall.java
@@ -3,16 +3,27 @@ package org.threadmonitoring.substitution.call;
 import org.threadmonitoring.substitution.LoggingNotifier;
 
 public class SynchronizedCall {
-    public static void alertSynchronizedEntry2() {
+
+    public static void alertBeforeSynchronizedEntry2(Object o) {
         LoggingNotifier.log(
-                "Thread " + Thread.currentThread().getName() + " entries the synchronized block"
+                "Thread " + Thread.currentThread().getName() + " tries to entry the synchronized block and " +
+                        "acquire the monitor of object " + o.toString()
                 , SynchronizedCall.class
                 , "INFO");
     }
 
-    public static void alertSynchronizedExit2() {
+    public static void alertSynchronizedEntry2(Object o) {
         LoggingNotifier.log(
-                "Thread " + Thread.currentThread().getName() + " exits the synchronized block"
+                "Thread " + Thread.currentThread().getName() + " entries the synchronized block and " +
+                        "acquires the monitor of object " + o.toString()
+                , SynchronizedCall.class
+                , "INFO");
+    }
+
+    public static void alertSynchronizedExit2(Object o) {
+        LoggingNotifier.log(
+                "Thread " + Thread.currentThread().getName() + " exits the synchronized block and " +
+                        "release the monitor of object " + o.toString()
                 , SynchronizedCall.class
                 , "INFO");
     }
