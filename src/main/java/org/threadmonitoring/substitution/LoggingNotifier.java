bdfb24b3efa087193f00159abbfd07acc921c2dc
PiotrStoklosa <PiotrJStoklosa@gmail.com>
2025-06-08 12:23:59 +0200

    Add priority logging

--- src/main/java/org/threadmonitoring/substitution/LoggingNotifier.java
+++ src/main/java/org/threadmonitoring/substitution/LoggingNotifier.java
@@ -2,19 +2,26 @@ package org.threadmonitoring.substitution;
 
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class LoggingNotifier {
 
+    private static final AtomicLong id = new AtomicLong(0);
     public static BlockingQueue<String> queue = new LinkedBlockingQueue<>();
 
-    public static void log(String message, Class<?> logger, String level) {
+    public static synchronized void log(String message, Class<?> logger, String level) {
+        long freshId = id.getAndIncrement();
+        String seq = String.format("%020d", freshId);
         queue.offer(
-                new StringBuilder(message)
+                new StringBuilder(seq)
+                        .append("###")
+                        .append(message)
                         .append("###")
                         .append(logger.toString())
                         .append("###")
                         .append(level)
-                        .toString());
+                        .toString()
+        );
     }
 
 }
