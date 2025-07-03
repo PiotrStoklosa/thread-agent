a875614f8bc7ea632abd004615ad55305006c55f
PiotrStoklosa <PiotrJStoklosa@gmail.com>
2025-07-03 18:52:59 +0200

    Add note to Bootstrapper

--- src/main/java/org/threadmonitoring/bootstrap/Bootstrapper.java
+++ src/main/java/org/threadmonitoring/bootstrap/Bootstrapper.java
@@ -4,7 +4,13 @@ import java.lang.invoke.CallSite;
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
-
+/*
+* Class loading handling, including the use of bootstrap methods,
+* in the Thread Agent partially uses an advanced dynamic method binding mechanism.
+* This allows classes loaded by class loaders other than the agent's to have access to interceptors.
+* This solution is inspired by the implementation in Elastic's apm-agent-java project:
+* https://github.com/elastic/apm-agent-java/blob/main/apm-agent-core/src/main/java/co/elastic/apm/agent/bci/IndyBootstrap.java#L77
+*/
 public class Bootstrapper {
 
     public static volatile MethodHandle bootstrap;
