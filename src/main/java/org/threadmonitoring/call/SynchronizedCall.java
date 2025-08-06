8b2b47c6f346783eef9c33756a28dac588a9db85
PiotrStoklosa <PiotrJStoklosa@gmail.com>
2025-08-06 21:07:26 +0200

    Remove already monitored methods from monitoring-call

--- src/main/java/org/threadmonitoring/call/SynchronizedCall.java
+++ src/main/java/org/threadmonitoring/call/SynchronizedCall.java
@@ -2,12 +2,6 @@ package org.threadmonitoring.call;
 
 public class SynchronizedCall {
 
-    public static void alertBeforeSynchronizedEntry(Object o) {
-    }
-
-    public static void alertSynchronizedEntry(Object o) {
-    }
-
-    public static void alertSynchronizedExit(Object o) {
+    public static void alertBeforeSynchronizedMethodEntry(Object monitor) {
     }
 }
