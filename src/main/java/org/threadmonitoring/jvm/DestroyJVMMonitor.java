626ded6ae25063782c0b34b4ad567be9f835440c
PiotrStoklosa <PiotrJStoklosa@gmail.com>
2025-08-07 18:25:13 +0200

    Add logging and stacktrace of running threads during termination to emergency log

--- src/main/java/org/threadmonitoring/jvm/DestroyJVMMonitor.java
+++ src/main/java/org/threadmonitoring/jvm/DestroyJVMMonitor.java
@@ -2,6 +2,7 @@ package org.threadmonitoring.jvm;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.threadmonitoring.logging.EmergencyLogger;
 
 import static org.threadmonitoring.advices.ThreadConstructorAdvice.threads;
 
@@ -17,13 +18,23 @@ public class DestroyJVMMonitor {
                     Thread.State state = thread.getState();
                     if (state != Thread.State.TERMINATED
                             && !"DestroyJavaVM".equals(thread.getName())
-                            && !"Thread-1".equals(thread.getName())) {
-                        LOGGER.warn("Thread {} was forced to terminate during JVM shutdown with state: {}",
-                                thread.getName(), state);
-                    }
+                            && !"Thread-1".equals(thread.getName())
+                            && !"Notification Thread".equals(thread.getName())
+                    ) {
 
+                        StackTraceElement[] stackTrace = thread.getStackTrace();
+                        StringBuilder sb = new StringBuilder();
+                        for (StackTraceElement element : stackTrace) {
+                            sb.append("\tat ").append(element).append("\n");
+                        }
+                        String message = "Thread " + thread.getName() +
+                                " was forced to terminate during JVM shutdown with state: " + state +
+                                "\nStacktrace:\n" + sb;
+                        EmergencyLogger.log(message);
+                    }
                 }
             }
+
         }));
     }
 
