5edd256aa479cf3baff92851298898725782f353
PiotrStoklosa <PiotrJStoklosa@gmail.com>
2025-09-01 11:50:52 +0200

    Add emergency log when wait without owning monitor

--- src/main/java/org/threadmonitoring/substitution/WaitSubstitution.java
+++ src/main/java/org/threadmonitoring/substitution/WaitSubstitution.java
@@ -3,12 +3,19 @@ package org.threadmonitoring.substitution;
 import org.threadmonitoring.analyzer.DeadlockAnalyzer;
 import org.threadmonitoring.logger.SynchronizedLogger;
 
+import static org.threadmonitoring.analyzer.DeadlockAnalyzer.isHoldingMonitor;
+import static org.threadmonitoring.substitution.LoggingNotifier.emergencyLog;
+
 public class WaitSubstitution {
     public static void wait2(Object o) throws InterruptedException {
         LoggingNotifier.log(
                 "Thread " + Thread.currentThread().getName() + " called wait() on object: " + o.toString()
                 , WaitSubstitution.class
                 , "INFO");
+        if (!isHoldingMonitor(o)) {
+            emergencyLog("Thread " + Thread.currentThread().getName() + " called wait() on object: " + o.toString() +
+                    " without holding the monitor on required object! Application will throw IllegalMonitorStateException");
+        }
         SynchronizedLogger.logExit(o);
         DeadlockAnalyzer.beforeWaitingForResource(Thread.currentThread(), o);
         o.wait();
@@ -19,4 +26,27 @@ public class WaitSubstitution {
                 , WaitSubstitution.class
                 , "INFO");
     }
+
+    public static void waitUntil2(Object o, long timeoutMillis) throws InterruptedException {
+        LoggingNotifier.log(
+                "Thread " + Thread.currentThread().getName() + " called wait()" + timeoutMillis +
+                        " on object: " + o.toString()
+                , WaitSubstitution.class
+                , "INFO");
+        if (!isHoldingMonitor(o)) {
+            emergencyLog("Thread " + Thread.currentThread().getName() + " called wait(" + timeoutMillis
+                    + ") on object: " + o.toString() +
+                    " without holding the monitor on required object! Application will throw IllegalMonitorStateException");
+        }
+        SynchronizedLogger.logExit(o);
+        DeadlockAnalyzer.beforeWaitingForResource(Thread.currentThread(), o);
+        o.wait(timeoutMillis);
+        SynchronizedLogger.logEnter2(o);
+        DeadlockAnalyzer.afterWaitingForResource(Thread.currentThread(), o, false);
+        LoggingNotifier.log(
+                "Thread " + Thread.currentThread().getName() + " got notified on object: " + o.toString()
+                , WaitSubstitution.class
+                , "INFO");
+    }
+
 }
